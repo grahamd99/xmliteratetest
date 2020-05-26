40e1cd31a9c070e9f114aee29c93dc17f5962571
Graham Dodsworth <graham.dodsworth@nhs.net>
2020-05-26 20:36:15 +0100

    update to iterate through multiple imm nodes

--- app.js
+++ app.js
@@ -14,43 +14,15 @@ var parser = new xml2js.Parser();
   parser.parseString(data, function (err, result) {
 
     console.dir(result);
-    console.log(util.inspect(result, false, null));
+    //console.log(util.inspect(result, false, null));
 
-/*
-    for(let val of result.Bundle.entry){
-      console.log(val)
+    numbVacc = result.Bundle.entry.length -1;
+    console.dir("Number of vacc to process: " + numbVacc);
+
+    for (i = 1; i <= numbVacc; i++) {
+      console.log("Immunization number: " + i)
+      console.log("Immunization value : " + result.Bundle.entry[i].resource[0].Immunization[0].name[0].$.value)
     }
-*/
-   global.stuff  = result.Bundle.entry[1].resource[0].Immunization[0].name[0].$.value;
-   //global.stuff  = result.Bundle;
-      
-      
-
-    //var myArray = result.Bundle.entry[];
-
-
-/*
-      global.identifierSystem   = result.Bundle.entry[1].resource[0].Immunization[0].identifier[0].system[0].$.value;
-      global.identifierValue    = result.Bundle.entry[1].resource[0].Immunization[0].identifier[0].value[0].$.value;
-      global.vaccineProcedureCode = result.Bundle.entry[1].resource[0].Immunization[0].extension[0].valueCodeableConcept[0].coding[0].code[0].$.value;
-      global.vaccineProcedureDisplay = result.Bundle.entry[1].resource[0].Immunization[0].extension[0].valueCodeableConcept[0].coding[0].display[0].$.value;     
-      global.notGiven           = result.Bundle.entry[1].resource[0].Immunization[0].notGiven[0].$.value;
-      global.vaccineCodeSNOMED  = result.Bundle.entry[1].resource[0].Immunization[0].vaccineCode[0].coding[0].code[0].$.value;
-      global.vaccineCodeDisplay = result.Bundle.entry[1].resource[0].Immunization[0].vaccineCode[0].coding[0].display[0].$.value;
-      global.dateChar           = result.Bundle.entry[1].resource[0].Immunization[0].date[0].$.value;
-      global.nhsnumber          = result.Bundle.entry[3].resource[0].Patient[0].identifier[0].value[0].$.value;
-      global.practitionerPrefix = result.Bundle.entry[6].resource[0].Practitioner[0].name[0].prefix[0].$.value;
-      global.practitionerGiven = result.Bundle.entry[6].resource[0].Practitioner[0].name[0].given[0].$.value;
-      global.practitionerFamily = result.Bundle.entry[6].resource[0].Practitioner[0].name[0].family[0].$.value;
-*/
-      //console.log( "type = " + type );
-      //console.log( "identifier (system) = " + identifierSystem );
-      //console.log( "identifier (value) = " + identifierValue );
-      //console.log( "notGiven = " + notGiven );
-      //console.log( "vaccineCode (value) = " + vaccineCodeSNOMED );
-      //console.log( "vaccineCode (display) = " + vaccineCodeDisplay );
-      //console.log( "dateChar = " + dateChar );
-      //console.log('Done');
 
   });
 })
